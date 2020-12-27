a6d9f95148471c5361b0182810d8f2d9a2c0c12d
Natalia Konovalova <ko.natalia.de@gmail.com>
2020-12-28 00:19:03 +0100

    fixed bugs with lower-apper case input

--- prog.py
+++ prog.py
@@ -8,7 +8,7 @@ myfile.close()
 # input word
 def inputword():
     myword = input('Please enter the word: ')
-    return myword.lower()
+    return print(translate(myword))
 #  check if input match key for min 80%
 def matchword(myword):
     match = get_close_matches(myword, data.keys(), n=1, cutoff = 0.8)
@@ -16,23 +16,30 @@ def matchword(myword):
         return match[0]
     else:
         return False
+# output string
+def output(myword, data):
+    str = 'The defenition of "{}": \n'.format(myword)
+    for item in data:
+        str = str + item +'\n'
+    return str
 # return right answer
 def translate(myword):
     if myword in data:
-        str = 'The defenition of "{}": \n'.format(myword)
-        for item in data[myword]:
-            str = str + item +'\n'
-        return str
-    elif matchword(myword):
-        myword = matchword(myword)
+        return output(myword, data[myword])
+    elif  myword.lower() in data:
+        return output(myword.lower(), data[myword.lower()])
+    elif  myword.upper() in data:
+        return output(myword.upper(), data[myword.upper()])
+    elif matchword(myword.lower()):
+        myword = matchword(myword.lower())
         print('Did you mean ' + myword + '?')
         answer = input('yes/no ')
         if answer == 'yes':
-            return 'The defenition of {}: {}'.format(myword, data[myword])
+            return output(myword, data[myword])
         else:
             inputword()
     else:
         return 'This word does not exist, please try again.'
-
+print('bla')
 myword = inputword()
-print(translate(myword))
+
